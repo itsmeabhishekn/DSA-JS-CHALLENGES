3c13e23ec9e7800e9ec9a18c3d73c69a7a1411e6
itsmeabhishekn <abhishekn4454@gmail.com>
2025-02-24 22:36:37 +0530

    title case program

---
+++ basics/06-title-case/titleCase-run.js
@@ -0,0 +1,4 @@
+const titleCase = require("./titleCase");
+titleCase("I'm a little tea pot"); // I'm A Little Tea Pot
+titleCase("sHoRt AnD sToUt"); // Short And Stout
+titleCase("HERE IS MY HANDLE HERE IS MY SPOUT"); // Here Is My Handle Here Is My Spout
