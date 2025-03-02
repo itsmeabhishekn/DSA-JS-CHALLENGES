83ad10fc5ec4574232539660916b8cabf397ad4d
itsmeabhishekn <abhishekn4454@gmail.com>
2025-03-02 12:38:26 +0530

    findMissingNumber program

---
+++ basics/14-find-missing-number/findMissingNumber.ts
@@ -0,0 +1,10 @@
+export default function findMissingNumber(arr: number[]): number {
+  if (!arr || arr.length) {
+    return 0;
+  }
+
+  const length = arr.length + 1;
+  const expectedSum = (length * (length + 1)) / 2;
+  const actualSum = arr.reduce((partialSum, a) => partialSum + a, 0);
+  return expectedSum - actualSum;
+}
